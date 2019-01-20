2e2cb423d6c7f53159342d31ed733ce5b99bf4a7
Jacob Leon Lyerla <umasour@yahoo.com>
2019-01-19 21:09:37 -0700

    Created a new build for testing this on netlify

---
+++ build/precache-manifest.5bd73f59a03c1499aedf7fdf9828d450.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3b2ebc7b885e8509a37a",
+    "url": "/static/js/main.3b2ebc7b.chunk.js"
+  },
+  {
+    "revision": "1b01b3af3cbce3c7fce8",
+    "url": "/static/js/1.1b01b3af.chunk.js"
+  },
+  {
+    "revision": "3b2ebc7b885e8509a37a",
+    "url": "/static/css/main.11db45c5.chunk.css"
+  },
+  {
+    "revision": "3c35528b084b2a573c943968053e108f",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+//Importing dependencies axios is used for http request.
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+
+  // adding a life cycle method to call the the database when the application is loaded
+  componentDidMount(
+    ){
+axios
+.get('https://api.themoviedb.org/3/search/movie',{
+    headers:{api_key:process.env.REACT_APP_TMDB_API_KEY,query:"Jack+Reacher"}
+  })
+  .then(response =>{
+    console.log(response.data)
+  })
+  }
+
   render() {
-    console.log(process.env.key)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          
-          <p>
-  
-          </p>
      
-    
-        </header>
       </div>
     );
   }
