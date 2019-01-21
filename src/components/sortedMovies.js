e79ca31f033c6a4fc0fbfeb219b3f073ffddddc9
Jacob Leon Lyerla <umasour@yahoo.com>
2019-01-20 23:38:18 -0700

    Added in buttons to change the types
    The types can now be changed by the buttons i added in , there is one for each type of sort

--- src/components/sortedMovies.js
+++ src/components/sortedMovies.js
@@ -1,21 +1,19 @@
-import React, { Component } from "react";
+import React, { Component,Fragment } from "react";
 // Using reactstrap and deconstructing the elements i want to use
 import { Media, Row, Col } from "reactstrap";
 
-class NewReleases extends Component {
+class SortedMovies extends Component {
   displayMovies = () => {
-    console.log(this.props.context.movieData.movies);
     if (this.props.context.movieData.movies !== undefined) {
       return this.props.context.movieData.movies.map(movie => (
-        <Col key={movie.title} md="1" style={{ minHeight: "25vh" }}>
+        <Col key={movie.id} md="1" style={{ minHeight: "25vh" }}>
           {/* Using my reactstrap Media component in order to render the jpg image that is stored,
           inside of the movie object, this is just a test however i would like to use this
           on the movie view, where we will show the single movie and it's details */}
-
-          <Media
+          {(movie.poster_path !==null)?(<Fragment><Media
             style={{ width: "5vw", margin: "0 auto" }}
             src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-          />
+          /></Fragment>):null}
           <div style={{ fontSize: "1vw", minHeight: "7vh" }}>{movie.title}</div>
         </Col>
       ));
@@ -23,7 +21,6 @@ class NewReleases extends Component {
   };
 
   render() {
-
     return (
       <div>
         <Row>{this.displayMovies()}</Row>
@@ -47,10 +44,42 @@ class NewReleases extends Component {
           >
             up
           </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center"
+            }}
+          >
+            <button
+              style={{ marginRight: ".6vw" }}
+              name="type"
+              value="now_playing"
+              onClick={this.props.context.actions.handleType}
+            >
+              Now Playing
+            </button>
+            <button
+              style={{ marginRight: ".6vw" }}
+              name="type"
+              value="top_rated"
+              onClick={this.props.context.actions.handleType}
+            >
+              Top Rated
+            </button>
+            <button
+              style={{ marginRight: ".6vw" }}
+              name="type"
+              value="popular"
+              onClick={this.props.context.actions.handleType}
+            >
+              Popular
+            </button>
+          </div>
         </div>
       </div>
     );
   }
 }
 
-export default NewReleases;
+export default SortedMovies;
