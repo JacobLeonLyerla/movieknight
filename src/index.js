e98ff969e74c0ea819ec02a57aa7a4dca7ef667d
Jacob <jacobleonlyerla@gmail.com>
2020-03-09 21:08:42 -0700

    formatting

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import "./index.css";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
+import {
+  BrowserRouter as Router
+} from "react-router-dom";
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+ReactDOM.render( <
+  Router >
+  <
+  App / >
+  <
+  /Router>,
   document.getElementById("root")
 );
